72645aa9447d2298f9fb06a81a2f114ed330f5ee
Décio Fernandes <gatofki@gmail.com>
2025-12-11 10:16:30 -0100

    chore: update dependencies and configure TypeScript support
    - Added @rollup/plugin-typescript to enable TypeScript compilation in Rollup.
    - Updated rollup.config.js to include TypeScript plugin in the build process.
    - Modified tsconfig.json to specify the ES2020 library for TypeScript.
    - Cleaned up package.json by removing duplicate ESLint dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,29 @@
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
 export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/index.esm.js',
-        format: 'es',
-        sourcemap: true
-      },
-      {
-        file: 'dist/index.cjs.js',
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'named'
-      }
-    ],
-    plugins: [resolve({ extensions: ['.js', '.ts'] }), commonjs()]
-  },
-  {
-    input: 'src/index.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'es' }],
-    plugins: [dts()]
-  }
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: 'dist/index.esm.js',
+                format: 'es',
+                sourcemap: true,
+            },
+            {
+                file: 'dist/index.cjs.js',
+                format: 'cjs',
+                sourcemap: true,
+                exports: 'named',
+            },
+        ],
+        plugins: [resolve({ extensions: ['.js', '.ts'] }), commonjs(), typescript({ tsconfig: './tsconfig.json' })],
+    },
+    {
+        input: 'src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()],
+    },
 ];
